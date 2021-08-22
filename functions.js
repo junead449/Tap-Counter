51f31208a403a7916bece3a4f1420007ee2838c7
AhmadRaza365 <ahmadrazabhatti30@gmail.com>
2021-08-23 00:52:50 +0500

    Tap Counter using JS
    Tap Counter using JS
    
    Just For Learning ;)

--- functions.js
+++ functions.js
@@ -1,12 +1,21 @@
 var count_num = 0;
 
 
-function tap() {
-    count(count_num);
+function increase() {
+    count_num++;
 document.getElementById("count_show").innerHTML = count_num;
 }
 
-function count() {
-count_num++;
+function decrease() {
+    if (count_num >0 ) {
+        
+    count_num--;
+    document.getElementById("count_show").innerHTML = count_num;
+    }
 
+}
+
+function reset() {
+    count_num = 0;
+    document.getElementById("count_show").innerHTML = count_num;
 }
